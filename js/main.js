265807ef5d997219065c25bed240c08a1f43a657
cdk <cdklinck@gmail.com>
2022-03-18 19:27:24 -0400

    change else

--- js/main.js
+++ js/main.js
@@ -14,32 +14,8 @@ let n = random(0,2)
   } else if (n === 1) {
     fill(r, g, 0)
     ellipse(100,200,100)
-  } else (n === 2) {
+  } else {
     fill(0, g, 0)
     ellipse(100,320,100)
   }
 }
-
-
-// fill(r, 0, 0)
-// ellipse(100,80,100)
-// fill(r, g, 0)
-// ellipse(100,200,100)
-// fill(0, g, 0)
-// ellipse(100,320,100)
-
-
-
-
-
-
-//  let x = width / 2
-// let y = height / 2
-//  let d = width / 4
-//
-//  let r = random(255)
-//  let g = random(255)
-//  let b = random(255)
-//  fill(r, g, b)
-//  ellipse(x, y, d)
-// }
